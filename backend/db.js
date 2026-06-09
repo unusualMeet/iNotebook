d4baf663fd0093615d93506b976db46a191d633b
meetmayani030 <meetmayani2006@gmail.com>
2026-06-09 14:05:23 +0530

    hello printed

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully!");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports = connectToMongo;
