0570548f2449b739a1aadbd833bacd237c63cb62
meetmayani030 <meetmayani2006@gmail.com>
2026-06-10 12:31:21 +0530

    authentication added in login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const JWT_SECRET='Meet is not a good b$oy at all'
-router.post("/",[
+router.post("/createuser",[
     body('email','Enter a valid email').isEmail(),
     body('name','Enter a valid name of atleast 3 characters').isLength({min:3}),
     body('password','Enter a valid password of atleast 5 characters').isLength({min:5})
@@ -38,11 +38,39 @@ router.post("/",[
             console.error(error.message)
             res.status(500).send("Some error occured")
         }
-        
-            
             // .then(user => res.json(user))
             // .catch(err=>{console.log("Error")})
             // res.json({error:'Please enter a unique email '})
         });
-
+router.post("/login",[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password can not be null').exists()
+],async (req, res) => {
+    const errors = validationResult(req);
+        if (!errors.isEmpty() ){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const {email,password}=req.body;
+        try {
+            let user =await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error:"Please try  to login with correct credential"})
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({error:"Please try  to login with correct credential"})
+            }
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const authToken=jwt.sign(data,JWT_SECRET);
+            res.json({authToken});
+        } 
+        catch(error){
+            console.error(error.message)
+            res.status(500).send("Internal server error ")
+        }
+})
 module.exports = router;
